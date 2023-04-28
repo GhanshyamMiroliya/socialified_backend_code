4339d9aa9ae68f48720a8af51c33c62821b5f58a
GhanshyamMiroliya <35294683+GhanshyamMiroliya@users.noreply.github.com>
2023-04-28 22:09:00 +0530

    IntialCommit

---
+++ common/config/main-local.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=social_feild',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
+                'username' => 'miroliyaghanshyam07@gmail.com',
+                'password' => 'rllphkkhpqelpvde',
+                'port' => '587', // Port 25 is a very common port too
+                'encryption' => 'tls', // It is often used, check your provider or mail server specs
+            ],
+        ],
+    ],
+];
